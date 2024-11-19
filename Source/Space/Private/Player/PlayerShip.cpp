74a44c1a98436cfbf650c55e5da4bae1cc55a212
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-11-19 02:40:01 -0500

    Player Class and BP creation

---
+++ Source/Space/Private/Player/PlayerShip.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/PlayerShip.h"
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+APlayerShip::APlayerShip()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ShipMesh = CreateDefaultSubobject<USkeletalMeshComponent>(FName(TEXT("ShipMesh")));
+	SetRootComponent(ShipMesh);
+}
+
+// Called when the game starts or when spawned
+void APlayerShip::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerShip::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerShip::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
