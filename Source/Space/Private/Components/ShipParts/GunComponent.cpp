20c2f875568c3af058efca2d4e8de4e3aec12f8c
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-11-22 16:00:25 -0500

    Gun component work

---
+++ Source/Space/Private/Components/ShipParts/GunComponent.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/ShipParts/GunComponent.h"
+
+#include "Components/ObjectPooling/PoolManagerComponent.h"
+#include "Editor/PropertyEditorTestObject.h"
+
+// Sets default values for this component's properties
+UGunComponent::UGunComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	if(ProjectilePool == nullptr)
+	{
+		ProjectilePool = CreateDefaultSubobject<UPoolManagerComponent>(FName("Projectile Pool"));
+	}
+}
+
+
+// Called when the game starts
+void UGunComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UGunComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	fireTimer -= DeltaTime;
+
+	// ...
+}
+
+void UGunComponent::FireGun()
+{
+	AActor* projectile =  ProjectilePool->GetPooledActor();
+	if(!projectile)return;
+	
+	projectile->SetActorLocation(GetComponentLocation());
+}
+
---
+++ Source/Space/Private/Components/ShipParts/GunComponent.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "GunComponent.generated.h"
+
+
+class UPoolManagerComponent;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UGunComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGunComponent();
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	UFUNCTION(BlueprintCallable)
+	void FireGun();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 RoundsPerMinute = 60;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Turret)
+	TObjectPtr<UPoolManagerComponent> ProjectilePool;
+private:
+	float fireTimer = 0;;
+};
